43e27b6fafb820f75c999af5a8896607c63c01da
julian-c-dig <julian@digroup.design>
2021-05-11 01:25:20 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,3 @@
+# Setting up variables, assuming the
+roomSideLong = input("Please enter the dimension of the longer side of the room.")
+roomSideShort = input("Please enter the dimesion of the shorter side of the room.")
