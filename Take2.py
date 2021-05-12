174fc08b8ca6c202c2d10bfdcda69f679ac56273
julian-c-dig <julian@digroup.design>
2021-05-12 00:30:44 -0400

    Take 2 Debugged, added vertical extra table if the aisle is wide enough

--- Take2.py
+++ Take2.py
@@ -1,29 +1,24 @@
 # Setting up variables
-def numTestPositive(Dim):
-    while True:
-        try:
-            val = int(Dim)
-            if val < 0:  # if not a positive int print message and ask for input again
-                print("Sorry, input must be a positive number, try again")
-                continue
-            break
-        except ValueError:
-            print("Please enter an positive number")
-    return val
+while True:
+    try:
+        roomSide1 = float(input("Please enter the first dimension of the room in US ft."))
+        roomSide2 = float(input("Please enter the second dimension of the room in US ft."))
+        if roomSide1 < 0 or roomSide2 < 0:
+            print("Sorry, input must be a positive number, try again")
+            continue
+        break
+    except ValueError:
+        print("Please enter an positive number")
 
-roomSide1 = numTestPositive(float(input("Please enter the first dimension of the room in US ft.")))
-roomSide2 = numTestPositive(float(input("Please enter the second dimension of the room in US ft.")))
 list = sorted([roomSide1, roomSide2])
-
-# For future adjustment of Non-orthogonal room
-roomWidth = list[0]
-roomLength = list[1]
+roomWidth = list[1]
+roomLength = list[0]
 
 # Desk size
-#deskSidesShort = float(input("Please enter the depth of the desk in US inch"))
-#deskSideLong = float(input("Please enter the width of the desk in US inch"))
+#deskSidesShort = numTestPositive(float(input("Please enter the depth of the desk in US inch")))
+#deskSideLong = numTestPositive((input("Please enter the width of the desk in US inch")))
 deskDepth = 24/12
-deskWidth = 80/12
+deskWidth = 48/12
 aisleWidth = 4
 minUnit = deskDepth + aisleWidth + deskDepth
 
@@ -38,6 +33,7 @@ longSideCount = deskRemainder(roomWidth, deskWidth)
 shortSideUnitCount = deskRemainder(roomLength, minUnit)
 roughTotal = longSideCount[0] * shortSideUnitCount[0] * 2
 aisleDeskCount = 0
+vDeskCount = 0
 
 if shortSideUnitCount[0] > 1:
     aisleDeskCount = shortSideUnitCount[0] * 2 - aisleWidth//deskWidth
@@ -45,11 +41,12 @@ if shortSideUnitCount[0] > 1:
 elif shortSideUnitCount[0] <= 1:
     aisleDeskCount = 0
 
-if shortSideUnitCount[1] >= aisleWidth + deskDepth: #For a row with no shared aisle.
+if shortSideUnitCount[1] >= aisleWidth + deskDepth: #Add a row with no shared aisle.
     roughTotal = roughTotal + longSideCount[0]
 
-if longSideCount[1] + deskWidth >= aisleWidth + deskDepth:  #For an extra table on the side if the aisle is wide enough.
-    roughTotal = roughTotal + shortSideUnitCount[0] - 1
+if longSideCount[1] >= deskDepth: #Add extra table on the side of double rows if the aisle is wide enough.
+    vDeskCount = shortSideUnitCount[0] - 1
+    roughTotal = roughTotal + vDeskCount
 
 deskCount = roughTotal - aisleDeskCount
 
@@ -57,3 +54,4 @@ print("You can fit " + str(int(deskCount)) + " desk in this room.")
 print("LongSideCount: " + str(longSideCount[0]), "Remainder: " + str(longSideCount[1]))
 print("shortSideUnitCount: " + str(shortSideUnitCount[0]), "Remainder: " + str(shortSideUnitCount[1]))
 print("aisleDeskCount: " + str(aisleDeskCount))
+print("vDeskCount: " + str(vDeskCount))
