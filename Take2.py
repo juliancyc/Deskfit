d644bcf9c3e69e09514d1170aa44035f4fffc7a4
Juliancyc <chuyun.j.cheng@gmail.com>
2021-05-21 14:24:01 -0400

    Rhino: Take 3

--- Take2.py
+++ Take2.py
@@ -9,7 +9,7 @@ while True:
         roomSide2 = float(input("Please enter the second dimension of the room in US ft."))
         deskSide1 = float(input("Please enter the first dimension of the desk in US inch"))
         deskSide2 = float(input("Please enter the second dimension of the desk in US inch"))
-        if roomSide1 * roomSide2 * deskSide1 * deskSide2 <= 0:
+        if roomSide1 * roomSide2 * deskSide1 * deskSide2 <= 0: #making sure the value is positive
             print("Sorry, input must be a positive number, try again")
             continue
         break
@@ -22,13 +22,12 @@ roomLength = list1[0]
 
 # Desk size
 list2 = sorted([deskSide1, deskSide2])
-deskDepth = list2[0]/12
+deskDepth = list2[0]/12 #from inch to foot
 deskWidth = list2[1]/12
-aisleWidth = 4
+aisleWidth = 4 #set to fix value but can be changed
 minUnit = deskDepth + aisleWidth + deskDepth
 
 #Function
-
 
 def deskRemainder(roomDim, deskDim):
     return divmod(roomDim, deskDim)
@@ -67,7 +66,16 @@ if longSideCount[1] + math.ceil(aisleWidth/deskWidth)*deskWidth >= aisleWidth + 
 
 deskCount = roughTotal - aisleDeskCount
 
+print("You can fit " + str(int(deskCount)) + " desk in this room.")
 
+print(list1, list2)
+print("LongSideCount: " + str(longSideCount[0]) + " desks", "Remainder: " + str(longSideCount[1]) + " ft")
+print("shortSideUnitCount: " + str(shortSideUnitCount[0])+" Min Units", "Remainder: " + str(shortSideUnitCount[1]) + " ft")
+print("aisleDeskCount: " + str(aisleDeskCount) + " desks")
+print("vDeskCount: " + str(vDeskCount) + " vertical desks")
+
+
+'''
 #Rhino Part
 model = rhino3dm.File3dm()
 deskList = []
@@ -84,41 +92,8 @@ roomPt = \
 
 model.Objects.AddPolyline(roomPt)
 
-#Desk Module
-desk = \
-    [[0, 0, 0],
-     [deskWidth, 0, 0],
-     [deskWidth, deskDepth, 0],
-     [0, deskDepth, 0],
-     [0, 0, 0]]
 
-#Functions
-
-def addY(p,n):
-    p[1] = p[1] + n
-    return p
-
-def addX(p,n):
-    p[0] = p[0] + n
-    return p
-
-
-def gen_row(d,i):
-    r = []
-    while i > 0:
-        new_d = []
-        for p in d:
-            new_p = addX(p,i)
-            new_d.append(new_p)
-        r.append(new_d)
-        i -= 1
-    return r
-
-print(gen_row(desk, 5))
-
-
-
-'''
+#Rhino:Take1  Desk Module to move/copy around 
 while i >= 1:
     deskModule = \
     [rhino3dm.Point3d(0, 0, 0),
@@ -133,7 +108,23 @@ for m in desks:
     model.Objects.AddPolyline(m)
 
 
+#Rhino Take2: Desk module + location calculation Ver1
+while i >= 1:
+    deskModule = \
+    [rhino3dm.Point3d(i * deskWidth, j*(deskDepth + aisleWidth), 0),
+        rhino3dm.Point3d((i - 1) * deskWidth, j*(deskDepth + aisleWidth + deskDepth), 0),
+        rhino3dm.Point3d((i - 1) * deskWidth, j*(deskDepth + aisleWidth), 0),
+        rhino3dm.Point3d(i * deskWidth, j*(deskDepth + aisleWidth + deskDepth), 0),
+        rhino3dm.Point3d(i * deskWidth, j*(deskDepth + aisleWidth), 0)]
 
+    deskList.append(deskModule)
+
+    i -= 1
+
+for m in deskList:
+    model.Objects.AddPolyline(m)
+
+#Rhino Take2: Desk module + location calculation Ver2
 while i >= 1:
     iList.append(i)
     i-=1
@@ -159,29 +150,43 @@ for d in deskList:
 model.Write('DeskPlan.3dm', 6)
 
 
-while i >= 1:
-    deskModule = \
-    [rhino3dm.Point3d(i * deskWidth, j*(deskDepth + aisleWidth), 0),
-        rhino3dm.Point3d((i - 1) * deskWidth, j*(deskDepth + aisleWidth + deskDepth), 0),
-        rhino3dm.Point3d((i - 1) * deskWidth, j*(deskDepth + aisleWidth), 0),
-        rhino3dm.Point3d(i * deskWidth, j*(deskDepth + aisleWidth + deskDepth), 0),
-        rhino3dm.Point3d(i * deskWidth, j*(deskDepth + aisleWidth), 0)]
 
-    deskList.append(deskModule)
+#Rhino:Take3 Simple Functions WIP
+#Desk Module
+desk = \
+    [[0, 0, 0],
+     [deskWidth, 0, 0],
+     [deskWidth, deskDepth, 0],
+     [0, deskDepth, 0],
+     [0, 0, 0]]
 
-    i -= 1
 
-for m in deskList:
-    model.Objects.AddPolyline(m)
+#Functions WIP, 
 
+def addY(p,n):
+    p[1] = p[1] + n
+    return p
+
+def addX(p,n):
+    p[0] = p[0] + n
+    return p
+
+
+def gen_row(d,i):
+    r = []
+    while i > 0:
+        new_d = []
+        for p in d:
+            new_p = addX(p,i)
+            new_d.append(new_p)
+        r.append(new_d)
+        i -= 1
+    return r
+
+
+
+#Baking/Generating Rhino 3dm file
 model.Write('DeskPlan.3dm', 6)
 '''
 
-print("You can fit " + str(int(deskCount)) + " desk in this room.")
-
-print(list1, list2)
-print("LongSideCount: " + str(longSideCount[0]) + " desks", "Remainder: " + str(longSideCount[1]) + " ft")
-print("shortSideUnitCount: " + str(shortSideUnitCount[0])+" Min Units", "Remainder: " + str(shortSideUnitCount[1]) + " ft")
-print("aisleDeskCount: " + str(aisleDeskCount) + " desks")
-print("vDeskCount: " + str(vDeskCount) + " vertical desks")
 
