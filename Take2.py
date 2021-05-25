54c30054d4445ba03d5e2d89866226d458f15cea
Juliancyc <chuyun.j.cheng@gmail.com>
2021-05-25 08:58:02 -0400

    Rhino: Take 3 genRow debugged

--- Take2.py
+++ Take2.py
@@ -9,14 +9,15 @@ while True:
         roomSide2 = float(input("Please enter the second dimension of the room in US ft."))
         deskSide1 = float(input("Please enter the first dimension of the desk in US inch"))
         deskSide2 = float(input("Please enter the second dimension of the desk in US inch"))
-        if roomSide1 * roomSide2 * deskSide1 * deskSide2 <= 0: #making sure the value is positive
+        list1 = sorted([roomSide1, roomSide2])
+        if roomSide1 * roomSide2 * deskSide1 <= 0 or deskSide2 <=0 : #making sure the value is positive
             print("Sorry, input must be a positive number, try again")
             continue
         break
     except ValueError:
         print("Please enter an positive number")
 
-list1 = sorted([roomSide1, roomSide2])
+
 roomWidth = list1[1]
 roomLength = list1[0]
 
@@ -33,15 +34,15 @@ def deskRemainder(roomDim, deskDim):
     return divmod(roomDim, deskDim)
 
 
-#Main
+#MainCalcs
 longSideCount = deskRemainder(roomWidth, deskWidth)
 shortSideUnitCount = deskRemainder(roomLength, minUnit)
 roughTotal = longSideCount[0] * shortSideUnitCount[0] * 2
 midRowCount = 0
 aisleDeskCount = 0
 vDeskCount = 0
-loneRow = False
-vDesk = False
+loneRow = False #for future visualization
+vDesk = False#for future visualization
 
 if shortSideUnitCount[0] == 1 and shortSideUnitCount[1] >= aisleWidth + deskDepth:
     aisleDeskCount = (shortSideUnitCount[0]) * math.ceil(aisleWidth/deskWidth)
@@ -125,7 +126,10 @@ while i >= 1:
 for m in deskList:
     model.Objects.AddPolyline(m)
 '''
+
+'''
 #Rhino Take2: Desk module + location calculation Ver2
+#"Maybe I can send in a list of points and solve everything in one For loop."
 
 i = longSideCount[0]
 j = shortSideUnitCount[0]
@@ -140,7 +144,7 @@ while j >= 1:
     jList.append(j)
     j-=1
 
-deskPt = itertools.product(iList,jList)
+deskPt = itertools.product(iList,jList) #produce a list of (row, column) location, and use it to generate all points needed for all desks
 
 for v in deskPt:
     desk = [rhino3dm.Point3d((v[0]-1) * deskWidth, (v[1]-1) * (deskDepth) + (v[1]-1)*(aisleWidth+deskDepth), 0),
@@ -154,9 +158,11 @@ for v in deskPt:
 for d in deskList:
     model.Objects.AddPolyline(d)
 
-model.Write('DeskPlan.3dm', 6)
-
+model.Write('DeskPlan.3dm', 6) #Generating Rhino3dm
 '''
+
+
+
 #Rhino:Take3 Simple Functions WIP
 #Desk Module
 desk = \
@@ -166,35 +172,31 @@ desk = \
      [0, deskDepth, 0],
      [0, 0, 0]]
 
+print(desk)
 
 #Functions WIP, 
 
-def addY(p,n):
-    p[1] = p[1] + n
-    return p
+def addPointY(p,n):
+    new_p = [p[0], p[1] + n, p[2]]
+    return new_p
 
-def addX(p,n):
-    p[0] = p[0] + n
-    return p
+def addPointX(p,n):
+    new_p = [p[0] + n, p[1], p[2]]
+    return new_p
 
 
 def gen_row(d,i):
     r = []
-    while i > 0:
+    l = [i * deskWidth for i in range (0,i)]
+    for n in l:
         new_d = []
         for p in d:
-            new_p = addX(p,i)
+            new_p = addPointX(p,n)
             new_d.append(new_p)
         r.append(new_d)
-        i -= 1
-    return r
+    return (r,l)
 
 
-
-
-'''
-#Baking/Generating Rhino 3dm file
-model.Write('DeskPlan.3dm', 6)
-
-
+print(gen_row(desk, 5))
+print(desk)
 
